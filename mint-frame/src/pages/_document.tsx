eadfd16fb0a79784f77e774d337cdf4223704155
Valerio <78494380+valeriofichera@users.noreply.github.com>
2024-03-09 20:28:52 +0000

    update image url

--- mint-frame/src/pages/_document.tsx
+++ mint-frame/src/pages/_document.tsx
@@ -6,9 +6,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta property="og:title" content={`Mint an NFT`} />
-        <meta property="og:image" content={`{config.hostUrl}/mintcaster_logo.png`} />
+        <meta property="og:image" content={`https://mintcaster-jade.vercel.app/mintcaster_logo.png`} />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={`{config.hostUrl}/mintcaster_logo.png`} />
+        <meta property="fc:frame:image" content={`https://mintcaster-jade.vercel.app/mintcaster_logo.png`} />
         <meta property="fc:frame:button:1" content="Get Started" />
         
       </Head>
